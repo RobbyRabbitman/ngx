b3fe04535cf417b1876501673649e2fd846fb3ab
robert hasemann <robert.hasemann@hdi.de>
2023-07-02 23:20:24 +0200

    feat(common): adds signal operators

--- libs/common/src/signal/iif.ts
+++ libs/common/src/signal/iif.ts
@@ -8,11 +8,16 @@ import { OperatorFunction, SignalOrRegularFn } from './types';
  * @param elseBranch
  * @returns the truthy branch if the source is truthy, else the falsy branch or undefined if not provided.
  */
-
-export const iif =
-  <C, T, F>(
-    thenBranch: SignalOrRegularFn<T>,
-    elseBranch?: SignalOrRegularFn<F>
-  ): OperatorFunction<C, T | F | undefined> =>
-  (source) =>
-    computed(() => (source() ? thenBranch() : elseBranch?.()));
+export function iif<C, T>(
+  thenBranch: SignalOrRegularFn<T>
+): OperatorFunction<C, T | undefined>;
+export function iif<C, T, F>(
+  thenBranch: SignalOrRegularFn<T>,
+  elseBranch: SignalOrRegularFn<F>
+): OperatorFunction<C, T | F>;
+export function iif<C, T, F>(
+  thenBranch: SignalOrRegularFn<T>,
+  elseBranch?: SignalOrRegularFn<F>
+): OperatorFunction<C, T | F | undefined> {
+  return (source) => computed(() => (source() ? thenBranch() : elseBranch?.()));
+}
