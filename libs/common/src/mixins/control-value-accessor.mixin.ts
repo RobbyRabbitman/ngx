ef4fbbfb1fe565f3e8b03f19dbd8f9c6dff05133
robert hasemann <robert.hasemann@hdi.de>
2023-07-06 11:16:55 +0200

    feat(common): mixin cva

--- libs/common/src/mixins/control-value-accessor.mixin.ts
+++ libs/common/src/mixins/control-value-accessor.mixin.ts
@@ -13,35 +13,90 @@ import { ControlValueAccessor, NgControl } from '@angular/forms';
 import { pairwise } from '../signal';
 import { noop } from '../util';
 
+/**
+ * This Directive mixes in a generic implemantion of the {@link ControlValueAccessor}.
+ */
 @Directive({
   standalone: true,
 })
 export class MixinControlValueAccessor<T> implements ControlValueAccessor {
-  private readonly _ngControl = inject(NgControl, {
+  /**
+   * The control directive. Is null if not present this host.
+   */
+  public readonly ngControl = inject(NgControl, {
     self: true,
     optional: true,
   });
 
-  private readonly onChange$ = signal<(value: T) => void>(noop);
+  /**
+   * This function is set by the forms api, if a control is present.
+   *
+   * @see {@link MixinControlValueAccessor.registerOnChange}
+   * @see {@link MixinControlValueAccessor.ngControl}
+   */
+  private readonly _onChange$ = signal<(value: T) => void>(noop);
 
-  private readonly onTouched$ = signal<() => void>(noop);
+  /**
+   * This function is set by the forms api, if a control is present.
+   *
+   * @see {@link MixinControlValueAccessor.registerOnTouched}
+   * @see {@link MixinControlValueAccessor.ngControl}
+   */
+  private readonly _onTouched$ = signal<() => void>(noop);
 
-  private readonly _disabled$ = signal(this._ngControl?.disabled ?? false);
+  /**
+   * This value is set by the forms api, if a control is present.
+   *
+   * @see {@link MixinControlValueAccessor.setDisabledState}
+   * @see {@link MixinControlValueAccessor.ngControl}
+   */
+  private readonly _disabled$ = signal(this.ngControl?.disabled ?? false);
 
-  private readonly _value$ = signal(this._ngControl?.value as T);
+  /**
+   * Internal value, which does not reflect {@link MixinControlValueAccessor.compareTo$}.
+   * In fact, this value is used to compute {@link MixinControlValueAccessor.value$}.
+   *
+   * @ignore
+   */
+  private readonly _value$ = signal(this.ngControl?.value as T);
 
+  /**
+   * The value of this mixin. If a control is present, it reflects it's value.
+   *
+   * @see {@link MixinControlValueAccessor.writeValue}
+   * @see {@link MixinControlValueAccessor.ngControl}
+   * @see {@link MixinControlValueAccessor.compareTo$}
+   */
   public readonly value$ = computed(() => {
     const [currentValue, newValue] = pairwise(this._value$())(this._value$)();
-    return this.compareTo$()(currentValue, newValue) ? newValue : currentValue;
+    return !this.compareTo$()(currentValue, newValue) ? newValue : currentValue;
   });
 
+  /**
+   * Whether this mixin is disabled. If a control is present, it reflects it's disabled state.
+   *
+   * @see {@link MixinControlValueAccessor.setDisabledState}
+   * @see {@link MixinControlValueAccessor.ngControl}
+   */
   public readonly disabled$ = this._disabled$.asReadonly();
 
-  public readonly compareTo$ = signal((a: T, b: T) => a !== b);
+  /**
+   * A comparator, which is used to determine {@link MixinControlValueAccessor.value$}. Defaults to strict equality.
+   * Should return true, if two values are considered semanticly equal.
+   */
+  public readonly compareTo$ = signal((a: T, b: T) => a === b);
 
-  public _updateControl$$ = effect(
+  /**
+   * Ensures the control is up to date with the UI of this host.
+   *
+   * @see {@link MixinControlValueAccessor.value$}
+   * @see {@link MixinControlValueAccessor._onChange$}
+   *
+   * @ignore
+   */
+  private readonly _updateControl$$ = effect(
     // If not put in next change detection cycle: NG100 ng-pristine: true -> false
-    () => requestAnimationFrame(() => this.onChange$()(this.value$()))
+    () => requestAnimationFrame(() => this._onChange$()(this.value$()))
   );
 
   @Input()
@@ -54,24 +109,35 @@ export class MixinControlValueAccessor<T> implements ControlValueAccessor {
     this.compareTo$.set(compareTo);
   }
 
+  /**
+   * An observable representing this {@link MixinControlValueAccessor.value$}.
+   */
   // eslint-disable-next-line @angular-eslint/no-output-rename
   @Output('valueChange')
   public readonly valueChange$ = toObservable(this.value$);
 
   public constructor() {
-    if (this._ngControl) this._ngControl.valueAccessor = this;
+    if (this.ngControl) this.ngControl.valueAccessor = this;
   }
 
+  /**
+   * This function should be called when this host is considered 'touched'. Has no effect without a present control.
+   *
+   * Whenever a 'blur' event is triggered on this host, this function gets called.
+   *
+   * @see {@link MixinControlValueAccessor.registerOnTouched}
+   * @see {@link MixinControlValueAccessor.ngControl}
+   */
   @HostListener('blur')
-  public blur = () => this.onTouched$()();
+  public blur = () => this._onTouched$()();
 
   // control value accessor
 
   public writeValue = (value: T) => this._value$.set(value);
 
-  public registerOnChange = (fn: (value: T) => void) => this.onChange$.set(fn);
+  public registerOnChange = (fn: (value: T) => void) => this._onChange$.set(fn);
 
-  public registerOnTouched = (fn: () => void) => this.onTouched$.set(fn);
+  public registerOnTouched = (fn: () => void) => this._onTouched$.set(fn);
 
   public setDisabledState = (disabled: boolean) =>
     this._disabled$.set(disabled);
