71469b445292f43eff5008d4fbe994bf78a1d6fa
robert hasemann <robert.hasemann@hdi.de>
2023-07-12 12:13:58 +0200

    feat(common): mixin cva

--- libs/common/src/signal/operators/combination/pairwise.ts
+++ libs/common/src/signal/operators/combination/pairwise.ts
@@ -1,5 +1,6 @@
 import { computed } from '@angular/core';
 import { OperatorFunction } from '../types';
+
 export function pairwise<T>(): OperatorFunction<T, [T | undefined, T]>;
 
 export function pairwise<T>(initialPrevious: T): OperatorFunction<T, [T, T]>;
