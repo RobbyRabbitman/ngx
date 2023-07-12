a8d98f74fed1f3cedc2896407549fa9fc3bc1ad5
Robby Rabbitman <robby.rabbitman+ngx@gmail.com>
2023-07-13 00:17:55 +0200

    build: adds coverage report

--- .github/workflows/update-coverage.sh
+++ .github/workflows/update-coverage.sh
@@ -2,6 +2,12 @@
 
 isAffected=$(eval "npx nx print-affected | node -pe 'JSON.parse(fs.readFileSync(0)).projects.length > 0'")
 
+tmpDir=${TEMP_DIR:-"./tmp"}
+
+mkdir -p $tmpDir
+
+echo creating tmp dir: $tmpDir
+
 if [ $isAffected = "false" ]
 then
   echo "nothing changed"
@@ -12,9 +18,15 @@ affected=$(eval "npx nx print-affected | node -pe 'JSON.parse(fs.readFileSync(0)
 
 echo "affected: $affected"
 
+mkdir -p $tmpDir
+
+cp -r ./coverage $tmpDir/coverage/
+
+git checkout -B artifacts origin/artifacts
 git restore --staged .
-git add -f ../../coverage
+cp -Tr $tmpDir/coverage/ coverage/
+git add .
 git commit -m "build(artifacts): update coverage for $affected"
-git push origin head:artifacts
+git push origin artifacts
 
 exit 0
