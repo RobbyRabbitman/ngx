a7e53042c29eb4ef2e62cf97046736db21c00eef
robert hasemann <robert.hasemann@hdi.de>
2023-07-12 12:49:17 +0200

    feat(common): mixin cva

--- jest.preset.js
+++ jest.preset.js
@@ -1,3 +1,13 @@
 const nxPreset = require('@nx/jest/preset').default;
 
-module.exports = { ...nxPreset };
+module.exports = {
+  ...nxPreset,
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 0,
+    },
+  },
+};
