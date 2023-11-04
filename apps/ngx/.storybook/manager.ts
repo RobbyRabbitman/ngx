9950ca6b752d74d1c2a232a78b31a0c5164dfec4
RobbyRabbitman <robby.rabbitman@gmail.com>
2023-11-04 01:57:52 +0100

    build: fixes ngx manager typing

--- apps/ngx/.storybook/manager.ts
+++ apps/ngx/.storybook/manager.ts
@@ -1,10 +1,11 @@
 import { addons } from '@storybook/manager-api';
+import { create } from '@storybook/theming/create';
 
 addons.setConfig({
-  theme: {
+  theme: create({
     base: 'dark',
     brandTitle: 'NGX',
     brandTarget: '_self',
     brandImage: 'favicon.ico',
-  },
+  }),
 });
